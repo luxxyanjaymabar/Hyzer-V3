ddd1a09957da21da68da9b76c7e0ee3d1ba4ca70
luxxyanjaymabar <110882963+luxxyanjaymabar@users.noreply.github.com>
2022-08-23 09:51:48 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -4,13 +4,13 @@ let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fet
 ╭═══════════════
 ║╭─── [ DONASI ] ───
 ║│➸ _*Dana*_✅ : 
-║│- 6282361160044
+║│- 6288210072756
 ║│
 ║│➸ _*Pulsa*_✅: 
-║│- 6285212023766
+║│- 6288210072756
 ║│
 ║│➸ _*OWNER*_
-║│- wa.me/6287892711054
+║│- wa.me/6288210072756
 ║╰────────────
 ╰═══════════════
 `.trim(), 'Donasi jan asal mencet', 'Owner Bot', '.owner')
